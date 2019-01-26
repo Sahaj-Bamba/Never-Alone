3f9d75131e3795d143b88da4b39b33b08e8959ce
Prashant Shakya <prashushakya@gmail.com>
2019-01-26 18:30:12 +0530

    changed activity_register.xml

--- app/src/main/java/com/example/hp/neveralone/RegisterActivity.java
+++ app/src/main/java/com/example/hp/neveralone/RegisterActivity.java
@@ -1,16 +1,15 @@
 package com.example.hp.neveralone;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.hp.neveralone.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -40,9 +39,9 @@ public class RegisterActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         username = findViewById(R.id.username);
-        email = findViewById(R.id.email);
+        email = findViewById(R.id.input_email);
         password = findViewById(R.id.password);
-        btn_register = findViewById(R.id.btn_register);
+        btn_register = findViewById(R.id.btn_signup);
 
         auth = FirebaseAuth.getInstance();
 
